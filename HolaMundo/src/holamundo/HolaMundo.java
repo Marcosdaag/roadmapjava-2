59415b4f12d1f2212c362748709bd4e0b84a4a69
Marcosdaag <marcosoffs99@gmail.com>
2026-07-18 01:45:40 -0300

    Scanner

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -4,6 +4,16 @@ public class HolaMundo {
 
     public static void main(String[] args) {
         System.out.println("Hola");
+        
+        // Variable sin asignacion de valor
+        int edad;
+        
+        // Variable con asignacion de valor
+        int precio = 50;
+        
+        // Las constantes son valores que no cambian
+        double impuesto = 1.25;
+        
+        System.out.println("El precio final es de " + precio*impuesto);
     }
-    
 }
