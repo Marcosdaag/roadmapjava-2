a8bc0bc30a1b7e5ba0deb799fbb3da6463256952
Marcosdaag <marcosoffs99@gmail.com>
2026-07-23 16:24:50 -0300

    Empezando ejercicio integrador

---
+++ TPIntegrador/src/main/java/logica/TPIntegrador.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package logica;
+
+/**
+ *
+ * @author Marcos
+ */
+public class TPIntegrador {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
