c7e0883e6185620b7ff6462a526d28671ee50bc7
Marcosdaag <marcosoffs99@gmail.com>
2026-07-24 14:56:27 -0300

    Contenedores Integrador

--- TPIntegrador/src/main/java/logica/TPIntegrador.java
+++ TPIntegrador/src/main/java/logica/TPIntegrador.java
@@ -5,6 +5,8 @@ import gui.Principal;
 public class TPIntegrador {
 
     public static void main(String[] args) {
+        
+        // varibles para la isntancia de interfaz y su locacion inicial
         Principal ventana = new Principal();
         ventana.setVisible(true);
         ventana.setLocationRelativeTo(null);
